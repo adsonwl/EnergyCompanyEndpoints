02c789d8e17c13c17ef8dd0e370148253ddc0ce1
adsonwl <adsonwl@gmail.com>
2022-04-21 11:21:36 -0300

    initial commit

---
+++ EnergyCompanyEndpoints/Entity/Enum/SwitchStateEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyCompanyEndpoints.Entity.Enum
+{
+    internal enum SwitchStateEnum
+    {
+        Disconnected = 0,
+        Connected = 1,
+        Armed = 2
+    }
+}
